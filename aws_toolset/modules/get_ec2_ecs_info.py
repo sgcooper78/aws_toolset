970faf6ee50622da785ec16c19851451362aa77c
Scott Cooper <sgcooper78@gmail.com>
2023-04-25 13:58:44 -0600

    add more logic

--- aws_toolset/modules/get_ec2_ecs_info.py
+++ aws_toolset/modules/get_ec2_ecs_info.py
@@ -15,75 +15,127 @@ def args_definitions(parser):
     # parser.add_argument("--ta","--targetaddress",dest="TargetAddress",help="REQUIRED: The Amazon Resource Name (ARN) of the Chatbot topic or Chatbot client.", required = True)
     #optional
     parser.add_argument("--c','--cluster",default='', dest="Cluster",help="Cluster to filter by")
-    parser.add_argument("--t','--taskname",dest="Task",help="Service Name to use to get EC2 information")
+    parser.add_argument("--cn','--containername",dest="ContainerName",help="Service Name to use to get EC2 information")
     # parser.add_argument("--c','--cluster",default='FULL', dest="DetailType", choices=['BASIC', 'FULL'],help="The level of detail to include in the notifications for this resource.")
 
-def run(args):
-    if not args.Task:
-        if not args.Cluster:
-            print("no user resource detected for cluster, helping user generate them")
-            clusters = get_all_resource_names("ecs","ecs_clusters")
-            # if not args.Cluster:
-            #     sys.exit("No Clusters detected")
-            cluster_questions = [
-                inquirer.List(
-                    "cluster",
-                    message="What Cluster would you like to use to narrow down services?",
-                    choices=clusters,
-                ),
-            ]
+def list_clusters():
+    paginator = ecs_client.get_paginator('list_clusters')
 
-            answers_cluster = inquirer.prompt(cluster_questions)
-            args.Cluster =  answers_cluster["cluster"]
+    all_resources_names = []
+    for page in paginator.paginate():
+        all_resources_names.extend(page["clusterArns"])
+
+    return all_resources_names
+
+def list_tasks(cluster):
+    paginator = ecs_client.get_paginator('list_tasks')
+
+    all_resources_names = []
+    for page in paginator.paginate(cluster=cluster):
+        all_resources_names.extend(page["taskArns"])
+
+    return all_resources_names
+
+def list_services(cluster):
+    paginator = ecs_client.get_paginator('list_services')
+
+    all_resources_names = []
+    for page in paginator.paginate(cluster=cluster):
+        all_resources_names.extend(page["serviceArns"])
+
+    return all_resources_names
+
+def list_tasks_with_containers(cluster,taskids):
+    
+    response = ecs_client.describe_tasks(
+        cluster=cluster,
+        tasks=taskids
+    )
+    container_names = {}
+    for task in response['tasks']:
+        task_id = task['taskArn']
+        container_name = next(iter(task['containers']))['name']
+        container_names[task_id] = {
+            'task_id': task_id,
+            'container_name': container_name
+        }
+    return container_names
+
+def describe_services(cluster,services):
+    
+    response = ecs_client.describe_services(
+        cluster=cluster,
+        services=services
+    )
+    return response
+
+def describe_task_container_name(cluster,taskid):
+    
+    response = ecs_client.describe_tasks(
+        cluster=cluster,
+        tasks=[
+            taskid,
+        ]
+    )
+
+    container_names = []
+    for container in response['tasks'][0]['containers']:
+        container_names.append(container['name'])
+    return container_names
+
+def get_tasks(cluster,service_name):
+    response = ecs_client.list_tasks(cluster=cluster, serviceName=service_name)
+    tasks = response['taskArns']
+
+    task_ids = []
+    for task_arn in tasks:
+        task_ids.append(task_arn.split("/")[-1])
+    return task_ids
+
+def question_single_list(list,type):
+    cluster_questions = [
+        inquirer.List(
+            "answer",
+            message=f"What {type} would you like to use?",
+            choices=list,
+        ),
+    ]
+
+    answers_cluster = inquirer.prompt(cluster_questions)
+    return answers_cluster["answer"]
+
+def question_single_confirm(Text,default):
+    questions = [
+        inquirer.Confirm("Confirm", message=Text,default=default),
+    ]
+    answers = inquirer.prompt(questions)
+    return answers["Confirm"]
+
+def question_single_text(Text):
+    questions = [
+        inquirer.Text("Text", message=Text),
+    ]
+
+    answers = inquirer.prompt(questions)
+    return answers['Text']
+
+def run(args):
+    if not args.Region:
+        print("no region detected, using default profile region")
+        args.Region = get_default_profile_region()
+        print(args.Region)
+    global ecs_client
+    ecs_client = boto3.client('ecs', region_name=args.Region)
+    if not args.Cluster:
+        print("no user resource detected for cluster, helping user generate them")
+
+        args.Cluster =  question_single_list(list_clusters(),"Cluster")
         
-        print(args.Cluster)
+        print(f"{args.Cluster} has been chosen")
         if not args.Cluster:
             sys.exit("No Cluster detected")
 
-        further_filter_question = [
-            inquirer.Confirm("filter", message=f"Would you like to filter down Tasks further than all in the cluster?", default=False),
-        ]
-        further_filter_bool = inquirer.prompt(further_filter_question)
-
-        tasks = []
-        if further_filter_bool["filter"]:
-            further_filter_resources_questions = [
-                inquirer.List(
-                    "choice",
-                    message="What would you like to filter resources down by?",
-                    choices=["resource_name","tags"],
-                    default=["resource_name"],
-                ),
-                #{"Environment":"Stage"}
-                inquirer.Text("filter_value", message="Enter what you want to filter down by? This is done by regex so doesn't need to be exact, for tags enter the exact tag example tag to enter would be "),
-            ]
-
-            filter_resources = inquirer.prompt(further_filter_resources_questions)
-                
-            if filter_resources["choice"] == "resource_name":
-                tasks = get_sort_resources_by_regex("ecs","ecs_tasks",filter_resources["filter_value"],{"cluster" : args.Cluster})
-            elif filter_resources["choice"] == "tags":
-                tasks = get_sort_resources_by_tag("ecs","ecs_tasks",json.loads(filter_resources["filter_value"]),{"cluster" : args.Cluster})
-            else:
-                tasks = get_all_resource_names("ecs","ecs_tasks",{"cluster" : args.Cluster})
-            print(tasks)
-        else:
-            tasks = get_all_resource_names("ecs","ecs_tasks",{"cluster" : args.Cluster})
-
-        task_question = [
-            inquirer.List(
-                "task",
-                message="What Task's would you like info about?",
-                choices=tasks,
-            ),
-        ]
-
-        answers_task = inquirer.prompt(task_question)
-        args.Task =  answers_task["task"]
-
     ec2_container_instances_arns = get_all_resource_names("ecs","ecs_container_instances",{"cluster" : args.Cluster})
-
-    ecs_client = boto3.client('ecs')
 
     ec2_container_instances = ecs_client.describe_container_instances(
         cluster=args.Cluster,
@@ -104,6 +156,24 @@ def run(args):
         )
 
         tasks.append(task_descriptions_response)
+    
+    
+    if not args.ContainerName:
+        print("no user resource detected for Container Name, helping user generate them")
+        container_names = describe_task_container_name(args.Cluster,args.TaskId)
+        # if not args.Cluster:
+        #     sys.exit("No Clusters detected")
+        if len(container_names) != 1:
+            args.ContainerName =  question_single_list(describe_task_container_name(args.Cluster,args.TaskId),"Container")
+        else:
+            print(f"Only one Container Detected, Container name {container_names[0]} will be used")
+            args.ContainerName = container_names[0]
+        
+        print(args.ContainerName)
+        if not args.ContainerName:
+            sys.exit("No container name detected")
+
+
     print(tasks)
 
 def main():
